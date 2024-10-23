a253ada317f38258dcc6250e62de9984852541c3
iheb cherni <eheebhob@gmail.com>
2024-10-23 17:52:03 +0100

    Fuction Basic

--- Python/Week_1/Day_1/Practice/Function basic 1/Functions Basic II/Functionbasic2.py
+++ Python/Week_1/Day_1/Practice/Function basic 1/Functions Basic II/Functionbasic2.py
@@ -15,3 +15,25 @@ def first_length(first):
 result=first_length([2,6,8,9])
 print(result)
 
+def values_greater_than_sec(liste1):
+    if len(liste1)<2:
+        return False
+    else :
+        newliste = []
+        count=0
+        for i in range(len(liste1)):
+            if liste1[i]>liste1[1]:
+                newliste.append(liste1[i])
+                count=count+1
+        print(count)
+        return newliste
+greater=values_greater_than_sec([6,3,2,9,0])
+print(greater)
+
+def This_length(num,repeat):
+    liste = []
+    for i in range(repeat):
+        liste.append(num)
+    return liste
+len=This_length(5,3)
+print(len)
