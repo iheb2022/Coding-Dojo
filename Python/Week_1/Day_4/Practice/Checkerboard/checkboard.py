b7f23d09725482fab45caffab64f7a82a5aabd84
iheb cherni <eheebhob@gmail.com>
2024-10-25 17:21:29 +0100

    HTML table

---
+++ Python/Week_1/Day_4/Practice/Checkerboard/checkboard.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app=Flask(__name__)
+
+@app.route('/')
+def default():
+  return render_template('index.html', insidebox=8, outsidebox=8)
+
+@app.route('/<int:outsidebox>')
+def outsidebox(outsidebox):
+  return render_template('index.html',insidebox=8, outsidebox=outsidebox)
+
+@app.route('/<int:insidebox>/<int:outsidebox>')
+def custombox(insidebox,outsidebox):
+  return render_template('index.html', insidebox=insidebox ,outsidebox=outsidebox)
+
+if __name__=="__main__":
+  app.run(debug=True)
+
